8e50e8274c46bd4e1b57ee1474bf5bf1c941263e
VoidNoi <voidnoi@proton.me>
2022-12-07 01:52:16 +0100

    got rid of code redundancy

--- Day4/script.js
+++ Day4/script.js
@@ -5,41 +5,40 @@ const sectionAssignments = fs
   .trim()
   .split('\n');
 
-const part1 = () => {
+const overlapChecker = (contains) => {
   let sumOverlap = 0;
   sectionAssignments.map((assignment) => {
     const splitSections = assignment.split(',');
     const firstGroup = splitSections[0].split('-');
     const secondGroup = splitSections[1].split('-');
-    if (
-      (parseInt(firstGroup[0]) <= parseInt(secondGroup[0]) &&
-        parseInt(firstGroup[1]) >= parseInt(secondGroup[1])) ||
-      (parseInt(secondGroup[0]) <= parseInt(firstGroup[0]) &&
-        parseInt(secondGroup[1]) >= parseInt(firstGroup[1]))
-    ) {
-      sumOverlap += 1;
+    if (contains === 'fullyContains') {
+      if (
+        (parseInt(firstGroup[0]) <= parseInt(secondGroup[0]) &&
+          parseInt(firstGroup[1]) >= parseInt(secondGroup[1])) ||
+        (parseInt(secondGroup[0]) <= parseInt(firstGroup[0]) &&
+          parseInt(secondGroup[1]) >= parseInt(firstGroup[1]))
+      ) {
+        sumOverlap += 1;
+      }
+    } else if (contains === 'overlaps') {
+      if (
+        (parseInt(secondGroup[0]) <= parseInt(firstGroup[1]) &&
+          parseInt(firstGroup[1]) <= parseInt(secondGroup[1])) ||
+        (parseInt(firstGroup[0]) <= parseInt(secondGroup[1]) &&
+          parseInt(secondGroup[1]) <= parseInt(firstGroup[1]))
+      ) {
+        sumOverlap += 1;
+      }
     }
   });
-
   return sumOverlap;
 };
-const part2 = () => {
-  let sumOverlap = 0;
-  sectionAssignments.map((assignment) => {
-    const splitSections = assignment.split(',');
-    const firstGroup = splitSections[0].split('-');
-    const secondGroup = splitSections[1].split('-');
 
-    if (
-      (parseInt(secondGroup[0]) <= parseInt(firstGroup[1]) &&
-        parseInt(firstGroup[1]) <= parseInt(secondGroup[1])) ||
-      (parseInt(firstGroup[0]) <= parseInt(secondGroup[1]) &&
-        parseInt(secondGroup[1]) <= parseInt(firstGroup[1]))
-    ) {
-      sumOverlap += 1;
-    }
-  });
-  return sumOverlap;
+const part1 = () => {
+  return overlapChecker('fullyContains');
+};
+const part2 = () => {
+  return overlapChecker('overlaps');
 };
 
 console.log(part1());
--- Day5/script.js
+++ Day5/script.js
@@ -25,22 +25,34 @@ for (let i = 0; i < rearrangement.length; i += chunkSize) {
   groups.push(chunk);
 }
 
-const part1 = () => {
+const reStack = (crateMover) => {
   const stacksCopy = stacks.map(function (arr) {
     return arr.slice();
   });
   let topCrates = '';
   groups.forEach((move) => {
-    let i = move[0];
-
+    let i = parseInt(move[0]);
+    let moveAmount = 0;
     while (i > 0) {
+      if (crateMover === 'crateMover9000') {
+        moveAmount = 1;
+      } else if (crateMover === 'crateMover9001') {
+        moveAmount = i;
+      }
       stacksCopy[move[2] - 1].push(
         stacksCopy[parseInt(move[1]) - 1][
-          stacksCopy[parseInt(move[1]) - 1].length - 1
+          stacksCopy[parseInt(move[1]) - 1].length - moveAmount
         ]
       );
+      if (crateMover === 'crateMover9000') {
+        stacksCopy[parseInt(move[1]) - 1].pop();
+      } else if (crateMover === 'crateMover9001') {
+        stacksCopy[parseInt(move[1]) - 1].splice(
+          stacksCopy[parseInt(move[1]) - 1].length - i,
+          1
+        );
+      }
 
-      stacksCopy[parseInt(move[1]) - 1].pop();
       i--;
     }
   });
@@ -50,34 +62,12 @@ const part1 = () => {
 
   return topCrates;
 };
+
+const part1 = () => {
+  return reStack('crateMover9000');
+};
 const part2 = () => {
-  let topCrates = '';
-  const stacksCopy = stacks.map(function (arr) {
-    return arr.slice();
-  });
-  groups.forEach((move) => {
-    let i = parseInt(move[0]);
-
-    while (i > 0) {
-      stacksCopy[move[2] - 1].push(
-        stacksCopy[parseInt(move[1]) - 1][
-          stacksCopy[parseInt(move[1]) - 1].length - i
-        ]
-      );
-
-      stacksCopy[parseInt(move[1]) - 1].splice(
-        stacksCopy[parseInt(move[1]) - 1].length - i,
-        1
-      );
-      i--;
-    }
-  });
-
-  stacksCopy.forEach((crates) => {
-    topCrates += crates[crates.length - 1].toString();
-  });
-
-  return topCrates;
+  return reStack('crateMover9001');
 };
 console.log(part1());
 console.log(part2());
